003c0561c104b7b4a2dce2d69dee1f5a52cf1e80
Lázaro Vanderson <eulazzo@protonmail.com>
2021-12-11 22:38:45 -0300

    tracking models

---
+++ ws/src/models/tracking.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Tracking = new mongoose.Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: [true, "User is required"],
+  },
+
+  challengeId: {
+    type: mongoose.Types.ObjectId,
+    ref: "Challenge",
+    required: [true, "Challenge is required"],
+  },
+
+  //F = Fee | G = Gain| L = Loss |
+  operation: {
+    type: String,
+    enum: ["F", "G", "L", "W"],
+    required: [true, "Operation is required"],
+  },
+
+  amount: {
+    type: Number,
+    required: [true, "Amount is required"],
+  },
+
+  transactionId: {
+    type: String,
+    required: [
+      function () {
+        return ["F", "W"].includes(this.operation);
+      },
+      "Transaction's ID is required",
+    ],
+  },
+
+  register: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default mongoose.model("Tracking", Tracking);
